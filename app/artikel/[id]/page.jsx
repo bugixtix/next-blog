573997ecd8ff8232a43bb3d8661257c32e761db7
Housam_ <ixtix_bug@hotmail.com>
2025-03-02 23:54:43 +0100

    Update: Component

--- app/artikel/[id]/page.jsx
+++ app/artikel/[id]/page.jsx
@@ -5,6 +5,10 @@ import Image from 'next/image'
 import {promises as fs} from "fs"
 import matter from "gray-matter"
 import process from 'process'
+import Markdown from "react-markdown"
+import rehypeHighlight from 'rehype-highlight'
+import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
 async function page({params}) {
   // const {id} = use(params);
   // const [content_, setContent_] = useState({
@@ -15,22 +19,30 @@ async function page({params}) {
   //   imgPath:"/example.jpg",
   //   text:"lorem ipsum"
   // })
-  const file  = await fs.readFile(process.cwd() + '/public/articles/1.md', 'utf8')
-
-  console.log(file)
+  const file  = await fs.readFile(process.cwd() + `/public/articles/${params?.id}.md`, 'utf-8')
+  var {data, content} = matter(file)
+  // meta = {meta}
+  const {id} = await params
+  // console.log(id)
+  console.dir(content ,{depth:null})
   return (
     <div className='w-[100%] flex flex-col items-center'>
-      <div className='w-[100%] md:w-[60%] p-4'>
+      <div className='w-[100%] lg:w-[60%] 2xl:w-[70%] p-4'>
 
-      {/* <div className=' flex flex-col items-center py-4'>
-        <p className='text-gray-500 italic text-sm'>{ARTICLES[id].date}</p>
-        <p className='font-semibold text-lg'>{ARTICLES[id].title}</p>
-        <p className='text-base/snug'>{ARTICLES[id].description}</p>
+      <div className=' flex flex-col items-center py-4'>
+        <p className='text-gray-500 italic text-sm'>{data?.date}</p>
+        <p className='font-semibold text-3xl my-4'>{data?.title}</p>
+        <p className='text-lg/snug'>{data?.description}</p>
       </div>
       <div className='w-[100%] py-1'>
-        <Image className='w-[100%] aspect-[1]' src={ARTICLES[id].imgPath} alt='bild' width={800} height={200} />
-      </div> */}
-      <div></div>
+        <Image className='w-[100%]' src={data?.cover} alt='bild' width={800} height={200} />
+      </div>
+      <div>
+        <Markdown rehypePlugins={[rehypeHighlight, rehypeRaw]} remarkPlugins={[remarkGfm]}>
+          {content}
+        </Markdown>
+      </div>
+
       </div>
     </div>
   )
