01dc44fe349c9ac0e5ee10d7dca811d3fd1dbadc
Housam_ <ixtix_bug@hotmail.com>
2025-03-27 19:41:22 +0100

    Update: Componente

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -21,26 +21,29 @@ function Navbar({theme = "light",setTheme=()=>{}, themeSwitcher=false}) {
       id:1,
       text:"Artikel",
       href:"/",
-      className:"text-lg font-semibold tracking-wide p-1 hover:text-purple-500 text-[rgb(var(--forderground))] transition-all ease-linear duration-300"
+      className:"text-lg font-semibold tracking-wide p-1 hover:text-purple-500 text-[rgb(var(--forderground))] transition-all ease-linear duration-300",
+      color:"text-purple-500"
     },
     {
       id:2,
       text:"Über Mich",
       href:"/uebermich",
-      className:"text-lg font-semibold tracking-wide p-1 hover:text-red-500 text-[rgb(var(--forderground))] transition-all ease-linear duration-300"
+      className:"text-lg font-semibold tracking-wide p-1 hover:text-red-500 text-[rgb(var(--forderground))] transition-all ease-linear duration-300",
+      color:"text-red-500"
     },
     {
       id:3,
       text:"Kontakt",
       href:"/kontakt",
       className:"text-lg font-semibold tracking-wide p-1 hover:text-slate-500 text-[rgb(var(--forderground))] transition-all ease-linear duration-300",
+      color:"text-slate-500"
     }
   ]
   return (
     <div className='w-[100%] flex items-center justify-center relative'>
         <div className='w-[100%] flex flex-wrap items-center justify-center gap-2 sm:gap-8 py-12'>
         {
-          ITEMS.map((i,index)=>(<a key={index} href={i.href} className={`${i.className} ${(i.href === pathname)&&"text-red-500"}`}>{i.text}</a>))
+          ITEMS.map((i,index)=>(<a key={index} href={i.href} className={`${i.className} ${(i.href === pathname)&& i.color}`}>{i.text}</a>))
         }
             {<button className='flex flex-wrap justify-center items-center group  transition-all ease-linear duration-300' onClick={()=>{HandleClick()}}>{ theme==="dark"?<Sun className={`text-2xl group-hover:text-yellow-400 text-[rgb(var(--forderground))] transition-all duration-300 ease-linear`}/>:<Moon className={`text-2xl text-[rgb(var(--forderground))] group-hover:text-blue-400 transition-all duration-300 ease-linear`}/>}</button>}
         </div>
--- app/kontakt/content.jsx
+++ app/kontakt/content.jsx
@@ -9,23 +9,26 @@ import { LiaLongArrowAltRightSolid as Arrow} from "react-icons/lia";
 import { TfiControlShuffle } from 'react-icons/tfi'
 
 export function ContactForm({}){
+    const btnTxt_ = {wait:"Ein Moment...", send:"Nachrich Senden", sendAgain:"Noch eine Nachricht senden"}
+
     const [formData, setFormData] = useState({name:"", message:"",email:"", type:"MESSAGE"})
-    const [btnTxt, setBtnTxt] = useState('Nachricht Senden')
+    const [btnTxt, setBtnTxt] = useState(btnTxt_.send)
+
     const DoChangeName = (e) =>{setFormData((p)=>({...p, name:e.target.value}))}
     const DoChangeMessage = (e) =>{setFormData((p)=>({...p, message:e.target.value}))}
     const DoChangeEmail = (e) =>{setFormData((p)=>({...p, email:e.target.value}))}
     const DoSubmit = (e) =>{
         e.preventDefault()
-        setBtnTxt('Ein Moment...')
+        setBtnTxt(btnTxt_.wait)
         const payload = {name:formData.name, email:formData.email, message:formData.message, type:formData.type}
         fetch('/api/backend', {method:'POST', headers:{'Content-Type':'application/json'}, body:JSON.stringify(payload)})
         .then((response)=>{return response.json()})
         .then((data)=>{
             console.log(data)
-            alert('succeed')
+            alert('Ich habe deine Nachricht bekommen, ich werde mich schnellstmöglich an Ihnen wenden.')
             if(data?.status === 200 ){
                 setFormData((p)=>({...p,email:"", name:"",message:""}))
-                setBtnTxt('Noch eine Nachricht Senden')
+                setBtnTxt(btnTxt_.sendAgain)
             }
         })
         .catch((error)=>{
@@ -40,26 +43,26 @@ export function ContactForm({}){
         email:"Email Adresse",
         emailPlaceholder:"Max.Mustermann@Beispiel.de",
         message:"Nachricht",
-        messagePlaceholder:"Hallo, mir gefällt ihr Blog und ...",
+        messagePlaceholder:"Hallo, ich möchte dich mal fragen ob du ...",
         button:"Nachricht Senden"
     }
     return(
         <div className='w-[100%] flex flex-col items-center' >
             <form onSubmit={DoSubmit} className='p-2 w-[100%] sm:w-[65%]'>
                 <div className='flex flex-col my-4'>
-                    <label htmlFor='input_name' className=' text-lg sm:text-xl text-[rgb(var(--text))]'>{text.name}</label>
+                    <label htmlFor='input_name' className=' text-lg sm:text-xl text-[rgb(var(--label))]'>{text.name}</label>
                     <input id='input_name' className='text-lg sm:text-xl ml-1 p-2 border-b-[1px] border-gray-200 font-light text-[rgb(var(--text))] placeholder:text-[rgb(var(--gray))]' type='text' required placeholder={text.namePlaceholder} value={formData.name} onChange={DoChangeName}/>
                 </div>
                 <div className='flex flex-col my-4'>
-                    <label htmlFor='input_email' className='text-lg sm:text-xl text-[rgb(var(--text))]'>{text.email}</label>
+                    <label htmlFor='input_email' className='text-lg sm:text-xl text-[rgb(var(--label))]'>{text.email}</label>
                     <input id='input_email' className=' text-lg sm:text-xl font-light ml-1 p-2 border-b-[1px] border-gray-200 text-[rgb(var(--text))] placeholder:text-[rgb(var(--gray))]' type='email' required placeholder={text.emailPlaceholder} value={formData.email} onChange={DoChangeEmail}/>
                 </div>
                 <div className='flex flex-col my-4'>
-                    <label htmlFor='textarea_message' className='text-lg sm:text-xl text-[rgb(var(--text))]'>{text.message}</label>
+                    <label htmlFor='textarea_message' className='text-lg sm:text-xl text-[rgb(var(--label))]'>{text.message}</label>
                     <textarea id='textarea_message' className='text-lg sm:text-xl  ml-1 p-2 border-b-[1px] border-gray-200 font-light text-[rgb(var(--text))] placeholder:text-[rgb(var(--gray))]' type='text' rows={5} required placeholder={text.messagePlaceholder} value={formData.message} onChange={DoChangeMessage} />
                 </div>
                 <div className='flex flex-row justify-end py-4'>
-                    <button className='flex flex-row gap-2 items-center text-lg sm:text-xl text-[rgb(var(--text))] '>{btnTxt} <Arrow className='text-2xl'/> </button>
+                    <button disabled={btnTxt===btnTxt_.wait} className='flex flex-row gap-2 items-center text-lg sm:text-xl text-[rgb(var(--text))] '>{btnTxt} <Arrow className='text-2xl'/> </button>
                 </div>
             </form>
         </div>
